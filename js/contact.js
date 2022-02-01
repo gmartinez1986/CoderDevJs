a1c1913a8525e073f2499f50491cecd430cbdae0
Gabriel Martinez <gabirio86@gmail.com>
2022-02-01 11:35:29 -0300

    Final Project - Validations - Contact

--- js/contact.js
+++ js/contact.js
@@ -1,20 +1,27 @@
-
 /****************************************************************
  * Vincular eventos a los distintos elementos según corresponda *
  ****************************************************************/
-$(document).ready(function() {
+$(document).ready(function () {
 
-    //----------------------------------------------------------------------------------------------
-    //Función que devuelve TRUE si el carácter evaluado es un numero, sino devuelve FALSE.
-    //Esta función se utiliza en evento "onkeypress" en los input de tipo text para que el usuario solo ingrese numeros.
-    $("#txtPhone").keypress(function(evt){
-        //Solo se permiten caracteres ASCII en ese rango.
-        var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    //Esta función se utiliza en evento "onkeypress".
+    //En los input de tipo text para que el usuario solo ingrese caracteres.
+    $("#txtName, #txtSurname").keypress(function (evt){
+
+        let ASCIICode = (evt.which) ? evt.which : evt.keyCode
+        if (!((ASCIICode > 64 && ASCIICode < 91) || (ASCIICode > 96 && ASCIICode < 123) || ASCIICode == 8))
+            evt.preventDefault();
+    });
+
+    //Esta función se utiliza en evento "onkeypress". 
+    //En los input de tipo text para que el usuario solo ingrese numeros.
+    $("#txtPhone").keypress(function (evt) {
+
+        let ASCIICode = (evt.which) ? evt.which : evt.keyCode
         if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
             evt.preventDefault();
     });
 
-    $("#sendContact").click(function(){
+    $("#sendContact").click(function () {
 
         let name = $('#txtName').val();
         let surname = $('#txtSurname').val();
@@ -22,18 +29,18 @@ $(document).ready(function() {
         let phone = $('#txtPhone').val();
         let newsLetter = $('#ckNewsLetter').checked;
         let comments = $('#txtComments').val();
-    
+
         //Objeto contacto.
         const contact1 = new Contact(name, surname, email, phone, newsLetter, comments);
-    
+
         //Validar info ingresada.
-        if(contact1.ValidateContact()){
+        if (contact1.ValidateContact()) {
 
             //Hago una llamada GET utilizando AJAX, para obtener los personajes de Harry Potter.
             $.ajax({
                 method: "GET",
                 url: "http://hp-api.herokuapp.com/api/characters",
-                success: function(res){
+                success: function (res) {
 
                     $('#frmContact').trigger("reset");
                     //Tomo el nombre del primer personaje.
@@ -56,27 +63,39 @@ $(document).ready(function() {
         }
 
         //Valido la información ingresada.
-        ValidateContact() { 
+        ValidateContact() {
 
-            if(this.name == ""){
+            if (this.name == "") {
                 alert("Debe ingresar un Nombre");
                 $('#txtName').focus();
                 return false;
             }
 
-            if(this.surname == ""){
+            if (this.name.length < 3) {
+                alert("El Nombre debe tener por lo menos 3 caracteres");
+                $('#txtName').focus();
+                return false;
+            }
+
+            if (this.surname == "") {
                 alert("Debe ingresar un Apellido");
                 $('#txtSurname').focus();
                 return false;
             }
 
-            if(this.email == ""){
+            if (this.surname.length < 3) {
+                alert("El Apellido debe tener por lo menos 3 caracteres");
+                $('#txtName').focus();
+                return false;
+            }
+
+            if (this.email == "") {
                 alert("Debe ingresar un Email");
                 $('#txtEmail').focus();
                 return false;
             }
 
-            if(this.phone == ""){
+            if (this.phone == "") {
                 alert("Debe ingresar un Telefono");
                 $('#txtPhone').focus();
                 return false;
