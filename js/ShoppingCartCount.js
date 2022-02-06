7a89a7fcddadf302a515e40f8607f708fca7f429
Gabriel Martinez <gabirio86@gmail.com>
2022-02-06 10:41:32 -0300

    ShoppingCartCount

---
+++ js/ShoppingCartCount.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    function renderCount(){
+
+        if(cart.length > 0){
+            $('#count').addClass("badge on");
+            $('#count').html(cart.length);
+        }
+    }
+
+    // Inicializo el carrito, en caso de que el Local Storege tenga un carrito pre cargado, uso la información pre cargada.
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    renderCount();
+});
